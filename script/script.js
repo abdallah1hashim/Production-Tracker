a3910b6cec21029db1c510247505b506647c9df0
Hashim <104575878+abdallah1hashim@users.noreply.github.com>
2023-12-08 23:28:52 +0200

    spl

--- script/script.js
+++ script/script.js
@@ -1,8 +1,5 @@
 "use strict";
 //////////////////////////////////
-const ids = [
-  494032, 422386, 853578, 709791, 500995, 326976, 767152, 804806, 768174,
-];
 // // Elements
 const footer = document.getElementsByTagName("footer")[0];
 const userInpt = document.querySelector(".log__user");
@@ -11,12 +8,19 @@ const loginForm = document.querySelector(".login");
 const app = document.querySelector(".app");
 const welcome = document.querySelector(".welcome");
 const navButton = document.querySelector(".login-btn");
-const closeLogin = document.querySelector(".close__login");
+const closeLogin = document.querySelector(".logout-btn");
 const overLay = document.querySelector(".overlay");
 const formCon = document.querySelector(".form-con");
 const wrngId = document.querySelector(".wrng__id");
 const wrngPass = document.querySelector(".wrng__pass");
-
+const main = document.querySelector("main");
+const info = document.querySelector(".info");
+const greating = document.querySelector(".greating");
+const search = document.querySelector(".search");
+const dayInpt = document.querySelector("#day");
+const monthInpt = document.querySelector("#month");
+const spl = document.querySelector(".spl");
+const splCon = document.querySelector(".spl-con");
 //////////////////////////////////
 //////////////////////////////////
 //////////////////////////////////
@@ -43,40 +47,144 @@ class User {
 
 const user1 = new User(
   422386,
-  "Myad Mohamed Mohamed",
+  "Eyad Mohamed Mohamed",
   "C-38",
-  2255,
+  4223,
+  "OverNight",
+  "Fourth Floor"
+);
+const user2 = new User(
+  494032,
+  "Mohamed saaid abd el aziz",
+  "C-37",
+  4940,
+  "OverNight",
+  "Fourth Floor"
+);
+const user3 = new User(
+  853578,
+  "Adel Sherif Abbas",
+  "C-38",
+  8535,
+  "OverNight",
+  "Fourth Floor"
+);
+const user4 = new User(
+  709791,
+  "Mahmoud youssry ghanem",
+  "C-38",
+  7097,
+  "OverNight",
+  "Fourth Floor"
+);
+const user5 = new User(
+  500995,
+  "Ahmed Ayman Mohamed",
+  "C-38",
+  5009,
+  "OverNight",
+  "Fourth Floor"
+);
+const user6 = new User(
+  767152,
+  "Ahmed Hussein Abdelreheem",
+  "C-38",
+  7671,
+  "OverNight",
+  "Fourth Floor"
+);
+const user7 = new User(
+  804806,
+  "Abdelrahman Alaa Abdelrasheed",
+  "C-38",
+  8048,
+  "OverNight",
+  "Fourth Floor"
+);
+const user8 = new User(
+  768174,
+  "fawzy sayd fawzy",
+  "C-38",
+  7681,
   "OverNight",
   "Fourth Floor"
 );
 users.push(user1);
+users.push(user2);
+users.push(user3);
+users.push(user4);
+users.push(user5);
+users.push(user6);
+users.push(user7);
+users.push(user8);
 //////////////////////////////////
 /////////////////
 
 //functions
+function getKeyByValue(object, value) {
+  return Object.keys(object).filter((key) => object[key] === value);
+}
 const RandomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
 const randColor = () =>
   `rgb(${RandomInt(0, 255)}, ${RandomInt(0, 255)}, ${RandomInt(0, 255)})`;
-const show = function(el) {
+const show = function (el) {
   el.classList.add("opacity-100", "z-pop");
-}
-const hide = function(el) {
+};
+const hide = function (el) {
   el.classList.remove("opacity-100", "z-pop");
   el.classList.add("opacity-0", "z-drop");
+};
+const showInfo = function (o, el) {
+  el.innerHTML = "";
+  [
+    ["Name:", o.name],
+    ["device:", o.device],
+    ["Floor:", o.floor],
+    ["Shift:", o.shift],
+  ].forEach((e) => {
+    const p = document.createElement("p");
+    p.textContent = `${e[0]} ${e[1]}`;
+    el.appendChild(p);
+  });
+};
+
+function greatingUser(name) {
+  name = name.split(" ")[0];
+  greating.textContent = `Welcome ${name}`;
 }
 
+function showSpl(m, d, id, el) {
+  el.innerHTML = "";
+  console.log(m.value, d.value);
+  const userSpl = allSpl[m.value]["spl" + d.value][id];
+  console.log(userSpl);
+  const userSplWQ = [
+    ["LIDAR", userSpl[0]],
+    ["LIDAR QA", userSpl[1]],
+    ["IMAGE", userSpl[2]],
+    ["IMAGE QA", userSpl[3]],
+  ];
+  userSplWQ.forEach((e) => {
+    const div = document.createElement("div");
+    div.classList.add("d-flex", "flex-column", "spl__item");
+    div.innerHTML = `
+      <p class="spl-label">${e[0]}</p>
+      <p>${e[1] == 0 ? "none" : Number(e[1]).toFixed(2)}</p>
+    `;
+    el.insertAdjacentElement("beforeend", div);
+  });
+}
 // Events
-footer.addEventListener("click", function() {
+footer.addEventListener("click", function () {
   this.style.backgroundColor = randColor();
 });
 
-
 navButton.addEventListener("click", () => {
   show(overLay);
-})
+});
 closeLogin.addEventListener("click", () => {
   hide(overLay);
-})
+});
 // overLay.addEventListener("click", (e)=> {
 //   if (e.target != formCon)
 //     hide(overLay);
@@ -84,30 +192,53 @@ closeLogin.addEventListener("click", () => {
 
 let us;
 
-loginForm.addEventListener("submit", function(e) {
+loginForm.addEventListener("submit", function (e) {
   e.preventDefault();
 
   if (!userInpt.value) return;
 
   if (ids.includes(+userInpt.value)) {
-
     us = users.find((u) => u.id === +userInpt.value);
+    console.log(us);
     const pass = +us.pin;
 
     if (+passInpt.value !== pass) {
-      console.log(passInpt)
+      console.log(passInpt);
       passInpt.classList.add("br-red");
       passInpt.focus();
       wrngPass.classList.add("opacity-100");
-    };
+    }
     if (+passInpt.value === pass) {
       console.log("logged in");
-      window.open("app.html", "_self");
+      // window.open("app.html", "_self");
+      hide(overLay);
+      main.classList.add("hidden");
+      main.classList.remove("d-flex");
+      app.classList.remove("hidden");
+      app.classList.add("d-flex");
+      navButton.classList.add("hidden");
+      closeLogin.classList.remove("hidden");
+      ///////////////////////////////////////
+      showInfo(us, info);
+      greatingUser(us.name);
     } else {
       console.log("smth wrong");
     }
-  }
-  else {
+  } else {
     wrngId.classList.add("opacity-100");
   }
+});
+
+closeLogin.addEventListener("click", function () {
+  console.log("clicked");
+  main.classList.remove("hidden");
+  main.classList.add("d-flex");
+  app.classList.add("hidden");
+  app.classList.remove("d-flex");
+  navButton.classList.remove("hidden");
+  closeLogin.classList.add("hidden");
+});
+
+search.addEventListener("click", function () {
+  showSpl(monthInpt, dayInpt, us.id, splCon);
 });
--- script/spl.js
+++ script/spl.js
@@ -1,356 +1,361 @@
-// "use strict";
+"use strict";
 
-// const months = [
-//   "January",
-//   "February",
-//   "March",
-//   "April",
-//   "May",
-//   "June",
-//   "July",
-//   "August",
-//   "September",
-//   "October",
-//   "November",
-//   "December",
-// ];
-// const ids = [
-//   494032, 422386, 853578, 709791, 500995, 326976, 767152, 804806, 768174,
-// ];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const ids = [
+  494032, 422386, 853578, 709791, 500995, 326976, 767152, 804806, 768174,
+];
 
-// const queuesName = [
-//   "perception_clm_deliverable_9",
-//   "A100 Full Scene Lidar Radar Daisy Max Range 200 Frames",
-//   "A110 Full Scene - Lidar + Radar, Max Range, 200 frames",
-//   "a110_surround_full_scene_lidar_image_150m_100_frames",
-//   "A100 DaisyML - Lidar, Daisy, 150m ROI, 200 frames",
-//   "A100 Full Scene - Lidar + Radar + Daisy + Image, Max Range, 200 frames",
-//   "A100 Full Scene Lidar Radar Daisy Max Range 100 Frames",
-//   "A100 Near Field - Lidar, Daisy, Image, 15m ROI, 200 frames",
-//   "A100 PoG - Lidar, Daisy, 150m ROI, 200 frames",
-//   "A100 Ultra Near Field Closed Course 10m 200 Frames",
-//   "A100 Ultra Near Field Closed Course 20m 100 Frames",
-//   "A100 Ultra Near Field Closed Course 20m 200 Frames",
-//   "A110 Construction and Lane Closed Zones - Lidar, 100m ROI, 200 frames",
-//   "A110 EMV - Lidar, 150m ROI (or track only), 200 frames",
-//   "A110 FP EMV - Lidar, 150m, 200 frames",
-//   "A110 Full Scene - Lidar + Image, 150m, 200 frames",
-//   "A110 Full-scene - Lidar, 150m ROI, 200 frames",
-//   "A110 Full-scene Parking Lots - Lidar, 150m ROI, 200 frames",
-//   "A110 Lane Closed - Lidar, 100m ROI, 200 frames",
-//   "A110 NCO Animals - Lidar + Image, 150m ROI, 200 frames",
-//   "A110 NCO Debris Lidar Image 150m 200 Frames",
-//   "A110 NCO FP - Lidar + Image, 150m ROI, 200 frames",
-//   "A110 Partial Scene CLM Lidar and Image 200 Frames",
-//   "A110 Small VRU - Lidar + Image, 100m ROI, 200 frames",
-//   "EMV MSL Human",
-//   "EMV MSL Search n Label",
-//   "FP school bus detection MSL",
-//   "a110_clm_full_scene_lidar_image_150m_100_frames",
-//   "a110_ultra_near_field_20m_200_frames",
-//   "emv_msl_human_high_pri",
-//   "fp_lrv_msl",
-// ];
-// const spl26nov = [
-//   [0, 0, 0, 6.072918919],
-//   [0, 1.297709841, 0, 7.825626634],
-//   [0.5183349027, 0, 0, 0],
-//   [0, 0, 0, 13.58275165],
-//   [0, 0, 0, 7.017251875],
-//   [0, 0.2886451966, 0, 0],
-//   [0.4417950512, 0, 0, 0],
-//   [0, 0, 0, 4.207866243],
-//   [0, 0, 0, 0],
-// ].map(function (row) {
-//   return row.map(function (cell) {
-//     return cell !== 0 ? cell : "";
-//   });
-// });
-// const spl27nov = [
-//   [0, 0, 0, 6.072918919],
-//   [0, 1.297709841, 0, 7.825626634],
-//   [0.5183349027, 0, 0, 0],
-//   [0, 0, 0, 13.58275165],
-//   [0, 0, 0, 7.017251875],
-//   [0, 0.2886451966, 0, 0],
-//   [0.4417950512, 0, 0, 0],
-//   [0, 0, 0, 4.207866243],
-//   [0, 0, 0, 0],
-// ].map(function (row) {
-//   return row.map(function (cell) {
-//     return cell !== 0 ? cell : "";
-//   });
-// });
-// const spl28nov = [
-//   [0, 0, 0, 6.072918919],
-//   [0, 1.297709841, 0, 7.825626634],
-//   [0.5183349027, 0, 0, 0],
-//   [0, 0, 0, 13.58275165],
-//   [0, 0, 0, 7.017251875],
-//   [0, 0.2886451966, 0, 0],
-//   [0.4417950512, 0, 0, 0],
-//   [0, 0, 0, 4.207866243],
-//   [0, 0, 0, 0],
-// ].map(function (row) {
-//   return row.map(function (cell) {
-//     return cell !== 0 ? cell : "";
-//   });
-// });
-// const spl29nov = [
-//   [0, 0, 0, 6.072918919],
-//   [0, 1.297709841, 0, 7.825626634],
-//   [0.5183349027, 0, 0, 0],
-//   [0, 0, 0, 13.58275165],
-//   [0, 0, 0, 7.017251875],
-//   [0, 0.2886451966, 0, 0],
-//   [0.4417950512, 0, 0, 0],
-//   [0, 0, 0, 4.207866243],
-//   [0, 0, 0, 0],
-// ].map(function (row) {
-//   return row.map(function (cell) {
-//     return cell !== 0 ? cell : "";
-//   });
-// });
-// const spl30nov = [
-//   [0, 0, 0, 6.072918919],
-//   [0, 1.297709841, 0, 7.825626634],
-//   [0.5183349027, 0, 0, 0],
-//   [0, 0, 0, 13.58275165],
-//   [0, 0, 0, 7.017251875],
-//   [0, 0.2886451966, 0, 0],
-//   [0.4417950512, 0, 0, 0],
-//   [0, 0, 0, 4.207866243],
-//   [0, 0, 0, 0],
-// ].map(function (row) {
-//   return row.map(function (cell) {
-//     return cell !== 0 ? cell : "";
-//   });
-// });
-// const spl1dec = [
-//   [0, 0, 0, 6.072918919],
-//   [0, 1.297709841, 0, 7.825626634],
-//   [0.5183349027, 0, 0, 0],
-//   [0, 0, 0, 13.58275165],
-//   [0, 0, 0, 7.017251875],
-//   [0, 0.2886451966, 0, 0],
-//   [0.4417950512, 0, 0, 0],
-//   [0, 0, 0, 4.207866243],
-//   [0, 0, 0, 0],
-// ].map(function (row) {
-//   return row.map(function (cell) {
-//     return cell !== 0 ? cell : "";
-//   });
-// });
-// const spl2dec = [
-//   [0, 0, 0, 6.072918919],
-//   [0, 1.297709841, 0, 7.825626634],
-//   [0.5183349027, 0, 0, 0],
-//   [0, 0, 0, 13.58275165],
-//   [0, 0, 0, 7.017251875],
-//   [0, 0.2886451966, 0, 0],
-//   [0.4417950512, 0, 0, 0],
-//   [0, 0, 0, 4.207866243],
-//   [0, 0, 0, 0],
-// ].map(function (row) {
-//   return row.map(function (cell) {
-//     return cell !== 0 ? cell : "";
-//   });
-// });
-// const spl3dec = [
-//   [0, 0, 0, 6.072918919],
-//   [0, 1.297709841, 0, 7.825626634],
-//   [0.5183349027, 0, 0, 0],
-//   [0, 0, 0, 13.58275165],
-//   [0, 0, 0, 7.017251875],
-//   [0, 0.2886451966, 0, 0],
-//   [0.4417950512, 0, 0, 0],
-//   [0, 0, 0, 4.207866243],
-//   [0, 0, 0, 0],
-// ].map(function (row) {
-//   return row.map(function (cell) {
-//     return cell !== 0 ? cell : "";
-//   });
-// });
-// const spl4dec = [
-//   [0, 0, 0, 6.072918919],
-//   [0, 1.297709841, 0, 7.825626634],
-//   [0.5183349027, 0, 0, 0],
-//   [0, 0, 0, 13.58275165],
-//   [0, 0, 0, 7.017251875],
-//   [0, 0.2886451966, 0, 0],
-//   [0.4417950512, 0, 0, 0],
-//   [0, 0, 0, 4.207866243],
-//   [0, 0, 0, 0],
-// ].map(function (row) {
-//   return row.map(function (cell) {
-//     return cell !== 0 ? cell : "";
-//   });
-// });
-// const spl5dec = [
-//   [0, 0, 0, 6.072918919],
-//   [0, 1.297709841, 0, 7.825626634],
-//   [0.5183349027, 0, 0, 0],
-//   [0, 0, 0, 13.58275165],
-//   [0, 0, 0, 7.017251875],
-//   [0, 0.2886451966, 0, 0],
-//   [0.4417950512, 0, 0, 0],
-//   [0, 0, 0, 4.207866243],
-//   [0, 0, 0, 0],
-// ].map(function (row) {
-//   return row.map(function (cell) {
-//     return cell !== 0 ? cell : "";
-//   });
-// });
-// const spl6dec = [
-//   [0, 0, 0, 6.072918919],
-//   [0, 1.297709841, 0, 7.825626634],
-//   [0.5183349027, 0, 0, 0],
-//   [0, 0, 0, 13.58275165],
-//   [0, 0, 0, 7.017251875],
-//   [0, 0.2886451966, 0, 0],
-//   [0.4417950512, 0, 0, 0],
-//   [0, 0, 0, 4.207866243],
-//   [0, 0, 0, 0],
-// ].map(function (row) {
-//   return row.map(function (cell) {
-//     return cell !== 0 ? cell : "";
-//   });
-// });
-// console.log(spl26nov);
-// const decSpl = {
-//   spl26nov: {
-//     month: `${months[10]}`,
-//     494032: spl26nov[0],
-//     422386: spl26nov[1],
-//     853578: spl26nov[2],
-//     709791: spl26nov[3],
-//     500995: spl26nov[4],
-//     326976: spl26nov[5],
-//     767152: spl26nov[6],
-//     804806: spl26nov[7],
-//     768174: spl26nov[8],
-//     hm: this,
-//   },
-//   spl27nov: {
-//     month: `${months[10]}`,
-//     494032: spl27nov[0],
-//     422386: spl27nov[1],
-//     853578: spl27nov[2],
-//     709791: spl27nov[3],
-//     500995: spl27nov[4],
-//     326976: spl27nov[5],
-//     767152: spl27nov[6],
-//     804806: spl27nov[7],
-//     768174: spl27nov[8],
-//   },
-//   spl28nov: {
-//     month: `${months[10]}`,
-//     494032: spl28nov[0],
-//     422386: spl28nov[1],
-//     853578: spl28nov[2],
-//     709791: spl28nov[3],
-//     500995: spl28nov[4],
-//     326976: spl28nov[5],
-//     767152: spl28nov[6],
-//     804806: spl28nov[7],
-//     768174: spl28nov[8],
-//   },
-//   spl29nov: {
-//     month: `${months[10]}`,
-//     494032: spl29nov[0],
-//     422386: spl29nov[1],
-//     853578: spl29nov[2],
-//     709791: spl29nov[3],
-//     500995: spl29nov[4],
-//     326976: spl29nov[5],
-//     767152: spl29nov[6],
-//     804806: spl29nov[7],
-//     768174: spl29nov[8],
-//   },
-//   spl30nov: {
-//     month: `${months[10]}`,
-//     494032: spl30nov[0],
-//     422386: spl30nov[1],
-//     853578: spl30nov[2],
-//     709791: spl30nov[3],
-//     500995: spl30nov[4],
-//     326976: spl30nov[5],
-//     767152: spl30nov[6],
-//     804806: spl30nov[7],
-//     768174: spl30nov[8],
-//   },
-//   spl1dec: {
-//     month: `${months[11]}`,
-//     494032: spl1dec[0],
-//     422386: spl1dec[1],
-//     853578: spl1dec[2],
-//     709791: spl1dec[3],
-//     500995: spl1dec[4],
-//     326976: spl1dec[5],
-//     767152: spl1dec[6],
-//     804806: spl1dec[7],
-//     768174: spl1dec[8],
-//   },
-//   spl2dec: {
-//     month: `${months[11]}`,
-//     494032: spl2dec[0],
-//     422386: spl2dec[1],
-//     853578: spl2dec[2],
-//     709791: spl2dec[3],
-//     500995: spl2dec[4],
-//     326976: spl2dec[5],
-//     767152: spl2dec[6],
-//     804806: spl2dec[7],
-//     768174: spl2dec[8],
-//   },
-//   spl3dec: {
-//     month: `${months[11]}`,
-//     494032: spl3dec[0],
-//     422386: spl3dec[1],
-//     853578: spl3dec[2],
-//     709791: spl3dec[3],
-//     500995: spl3dec[4],
-//     326976: spl3dec[5],
-//     767152: spl3dec[6],
-//     804806: spl3dec[7],
-//     768174: spl3dec[8],
-//   },
-//   spl4dec: {
-//     month: `${months[11]}`,
-//     494032: spl4dec[0],
-//     422386: spl4dec[1],
-//     853578: spl4dec[2],
-//     709791: spl4dec[3],
-//     500995: spl4dec[4],
-//     326976: spl4dec[5],
-//     767152: spl4dec[6],
-//     804806: spl4dec[7],
-//     768174: spl4dec[8],
-//   },
-//   spl5dec: {
-//     month: `${months[11]}`,
-//     494032: spl5dec[0],
-//     422386: spl5dec[1],
-//     853578: spl5dec[2],
-//     709791: spl5dec[3],
-//     500995: spl5dec[4],
-//     326976: spl5dec[5],
-//     767152: spl5dec[6],
-//     804806: spl5dec[7],
-//     768174: spl5dec[8],
-//   },
-//   spl6dec: {
-//     month: `${months[11]}`,
-//     494032: spl6dec[0],
-//     422386: spl6dec[1],
-//     853578: spl6dec[2],
-//     709791: spl6dec[3],
-//     500995: spl6dec[4],
-//     326976: spl6dec[5],
-//     767152: spl6dec[6],
-//     804806: spl6dec[7],
-//     768174: spl6dec[8],
-//   },
-// };
+const queuesName = [
+  "perception_clm_deliverable_9",
+  "A100 Full Scene Lidar Radar Daisy Max Range 200 Frames",
+  "A110 Full Scene - Lidar + Radar, Max Range, 200 frames",
+  "a110_surround_full_scene_lidar_image_150m_100_frames",
+  "A100 DaisyML - Lidar, Daisy, 150m ROI, 200 frames",
+  "A100 Full Scene - Lidar + Radar + Daisy + Image, Max Range, 200 frames",
+  "A100 Full Scene Lidar Radar Daisy Max Range 100 Frames",
+  "A100 Near Field - Lidar, Daisy, Image, 15m ROI, 200 frames",
+  "A100 PoG - Lidar, Daisy, 150m ROI, 200 frames",
+  "A100 Ultra Near Field Closed Course 10m 200 Frames",
+  "A100 Ultra Near Field Closed Course 20m 100 Frames",
+  "A100 Ultra Near Field Closed Course 20m 200 Frames",
+  "A110 Construction and Lane Closed Zones - Lidar, 100m ROI, 200 frames",
+  "A110 EMV - Lidar, 150m ROI (or track only), 200 frames",
+  "A110 FP EMV - Lidar, 150m, 200 frames",
+  "A110 Full Scene - Lidar + Image, 150m, 200 frames",
+  "A110 Full-scene - Lidar, 150m ROI, 200 frames",
+  "A110 Full-scene Parking Lots - Lidar, 150m ROI, 200 frames",
+  "A110 Lane Closed - Lidar, 100m ROI, 200 frames",
+  "A110 NCO Animals - Lidar + Image, 150m ROI, 200 frames",
+  "A110 NCO Debris Lidar Image 150m 200 Frames",
+  "A110 NCO FP - Lidar + Image, 150m ROI, 200 frames",
+  "A110 Partial Scene CLM Lidar and Image 200 Frames",
+  "A110 Small VRU - Lidar + Image, 100m ROI, 200 frames",
+  "EMV MSL Human",
+  "EMV MSL Search n Label",
+  "FP school bus detection MSL",
+  "a110_clm_full_scene_lidar_image_150m_100_frames",
+  "a110_ultra_near_field_20m_200_frames",
+  "emv_msl_human_high_pri",
+  "fp_lrv_msl",
+];
+const allSpl = {};
+const spl26nov = [
+  [0, 0, 6.072918919, 0],
+  [0, 1.297709841, 7.825626634, 0],
+  [0.5183349027, 0, 0, 0],
+  [0, 0, 13.58275165, 0],
+  [0, 0, 7.017251875, 0],
+  [0, 0.2886451966, 0, 0],
+  [0.4417950512, 0, 0, 0],
+  [0, 0, 4.207866243, 0],
+  [0, 0, 0, 0],
+].map(function (row) {
+  return row.map(function (cell) {
+    return cell !== 0 ? cell : "";
+  });
+});
+const spl27nov = [
+  [0, 0, 0, 3.970672647],
+  [0, 0.4132066889, 0, 0],
+  [0.5203480058, 0, 0, 0],
+  [0.3741663793, 0, 0, 0],
+  [0.2923560735, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0.3890450722, 0, 0, 0],
+  [0.1256956249, 0, 0, 1.233875729],
+  [0, 0, 0, -44.12193035],
+].map(function (row) {
+  return row.map(function (cell) {
+    return cell !== 0 ? cell : "";
+  });
+});
+const spl28nov = [
+  [0, 0, 0, 6.072918919],
+  [0, 1.297709841, 0, 7.825626634],
+  [0.5183349027, 0, 0, 0],
+  [0, 0, 0, 13.58275165],
+  [0, 0, 0, 7.017251875],
+  [0, 0.2886451966, 0, 0],
+  [0.4417950512, 0, 0, 0],
+  [0, 0, 0, 4.207866243],
+  [0, 0, 0, 0],
+].map(function (row) {
+  return row.map(function (cell) {
+    return cell !== 0 ? cell : "";
+  });
+});
+const spl29nov = [
+  [0, 0, 0, 6.072918919],
+  [0, 1.297709841, 0, 7.825626634],
+  [0.5183349027, 0, 0, 0],
+  [0, 0, 0, 13.58275165],
+  [0, 0, 0, 7.017251875],
+  [0, 0.2886451966, 0, 0],
+  [0.4417950512, 0, 0, 0],
+  [0, 0, 0, 4.207866243],
+  [0, 0, 0, 0],
+].map(function (row) {
+  return row.map(function (cell) {
+    return cell !== 0 ? cell : "";
+  });
+});
+const spl30nov = [
+  [0, 0, 0, 6.072918919],
+  [0, 1.297709841, 0, 7.825626634],
+  [0.5183349027, 0, 0, 0],
+  [0, 0, 0, 13.58275165],
+  [0, 0, 0, 7.017251875],
+  [0, 0.2886451966, 0, 0],
+  [0.4417950512, 0, 0, 0],
+  [0, 0, 0, 4.207866243],
+  [0, 0, 0, 0],
+].map(function (row) {
+  return row.map(function (cell) {
+    return cell !== 0 ? cell : "";
+  });
+});
+const spl1dec = [
+  [0, 0, 0, 6.072918919],
+  [0, 1.297709841, 0, 7.825626634],
+  [0.5183349027, 0, 0, 0],
+  [0, 0, 0, 13.58275165],
+  [0, 0, 0, 7.017251875],
+  [0, 0.2886451966, 0, 0],
+  [0.4417950512, 0, 0, 0],
+  [0, 0, 0, 4.207866243],
+  [0, 0, 0, 0],
+].map(function (row) {
+  return row.map(function (cell) {
+    return cell !== 0 ? cell : "";
+  });
+});
+const spl2dec = [
+  [0, 0, 0, 6.072918919],
+  [0, 1.297709841, 0, 7.825626634],
+  [0.5183349027, 0, 0, 0],
+  [0, 0, 0, 13.58275165],
+  [0, 0, 0, 7.017251875],
+  [0, 0.2886451966, 0, 0],
+  [0.4417950512, 0, 0, 0],
+  [0, 0, 0, 4.207866243],
+  [0, 0, 0, 0],
+].map(function (row) {
+  return row.map(function (cell) {
+    return cell !== 0 ? cell : "";
+  });
+});
+const spl3dec = [
+  [0, 0, 0, 6.072918919],
+  [0, 1.297709841, 0, 7.825626634],
+  [0.5183349027, 0, 0, 0],
+  [0, 0, 0, 13.58275165],
+  [0, 0, 0, 7.017251875],
+  [0, 0.2886451966, 0, 0],
+  [0.4417950512, 0, 0, 0],
+  [0, 0, 0, 4.207866243],
+  [0, 0, 0, 0],
+].map(function (row) {
+  return row.map(function (cell) {
+    return cell !== 0 ? cell : "";
+  });
+});
+const spl4dec = [
+  [0, 0, 0, 6.072918919],
+  [0, 1.297709841, 0, 7.825626634],
+  [0.5183349027, 0, 0, 0],
+  [0, 0, 0, 13.58275165],
+  [0, 0, 0, 7.017251875],
+  [0, 0.2886451966, 0, 0],
+  [0.4417950512, 0, 0, 0],
+  [0, 0, 0, 4.207866243],
+  [0, 0, 0, 0],
+].map(function (row) {
+  return row.map(function (cell) {
+    return cell !== 0 ? cell : "";
+  });
+});
+const spl5dec = [
+  [0, 0, 0, 6.072918919],
+  [0, 1.297709841, 0, 7.825626634],
+  [0.5183349027, 0, 0, 0],
+  [0, 0, 0, 13.58275165],
+  [0, 0, 0, 7.017251875],
+  [0, 0.2886451966, 0, 0],
+  [0.4417950512, 0, 0, 0],
+  [0, 0, 0, 4.207866243],
+  [0, 0, 0, 0],
+].map(function (row) {
+  return row.map(function (cell) {
+    return cell !== 0 ? cell : "";
+  });
+});
+const spl6dec = [
+  [0.1666479678, 0.05931224089, 2.056832418, 0],
+  [0.08479180346, 0.07200493737, 0, 0],
+  [0.2300630502, 0, 0, 0],
+  [0.2300630502, 0, 0, 0],
+  [0.1705829633, 0, 0, 8.822932156],
+  [0, 0, 0, 0],
+  [0.257119244, 0, 0, 0],
+  [0.1043436217, 0, 0, 0],
+  [0.1381339199, 0, 0, 1.306797399],
+].map(function (row) {
+  return row.map(function (cell) {
+    return cell !== 0 ? cell : "";
+  });
+});
+console.log(spl26nov);
+const novSpl = {
+  spl26: {
+    month: `${months[10]}`,
+    494032: spl26nov[0],
+    422386: spl26nov[1],
+    853578: spl26nov[2],
+    709791: spl26nov[3],
+    500995: spl26nov[4],
+    326976: spl26nov[5],
+    767152: spl26nov[6],
+    804806: spl26nov[7],
+    768174: spl26nov[8],
+    hm: this,
+  },
+  spl27: {
+    month: `${months[10]}`,
+    494032: spl27nov[0],
+    422386: spl27nov[1],
+    853578: spl27nov[2],
+    709791: spl27nov[3],
+    500995: spl27nov[4],
+    326976: spl27nov[5],
+    767152: spl27nov[6],
+    804806: spl27nov[7],
+    768174: spl27nov[8],
+  },
+  spl28: {
+    month: `${months[10]}`,
+    494032: spl28nov[0],
+    422386: spl28nov[1],
+    853578: spl28nov[2],
+    709791: spl28nov[3],
+    500995: spl28nov[4],
+    326976: spl28nov[5],
+    767152: spl28nov[6],
+    804806: spl28nov[7],
+    768174: spl28nov[8],
+  },
+  spl29: {
+    month: `${months[10]}`,
+    494032: spl29nov[0],
+    422386: spl29nov[1],
+    853578: spl29nov[2],
+    709791: spl29nov[3],
+    500995: spl29nov[4],
+    326976: spl29nov[5],
+    767152: spl29nov[6],
+    804806: spl29nov[7],
+    768174: spl29nov[8],
+  },
+  spl30: {
+    month: `${months[10]}`,
+    494032: spl30nov[0],
+    422386: spl30nov[1],
+    853578: spl30nov[2],
+    709791: spl30nov[3],
+    500995: spl30nov[4],
+    326976: spl30nov[5],
+    767152: spl30nov[6],
+    804806: spl30nov[7],
+    768174: spl30nov[8],
+  },
+};
+const decSpl = {
+  spl1: {
+    month: `${months[11]}`,
+    494032: spl1dec[0],
+    422386: spl1dec[1],
+    853578: spl1dec[2],
+    709791: spl1dec[3],
+    500995: spl1dec[4],
+    326976: spl1dec[5],
+    767152: spl1dec[6],
+    804806: spl1dec[7],
+    768174: spl1dec[8],
+  },
+  spl2: {
+    month: `${months[11]}`,
+    494032: spl2dec[0],
+    422386: spl2dec[1],
+    853578: spl2dec[2],
+    709791: spl2dec[3],
+    500995: spl2dec[4],
+    326976: spl2dec[5],
+    767152: spl2dec[6],
+    804806: spl2dec[7],
+    768174: spl2dec[8],
+  },
+  spl3: {
+    month: `${months[11]}`,
+    494032: spl3dec[0],
+    422386: spl3dec[1],
+    853578: spl3dec[2],
+    709791: spl3dec[3],
+    500995: spl3dec[4],
+    326976: spl3dec[5],
+    767152: spl3dec[6],
+    804806: spl3dec[7],
+    768174: spl3dec[8],
+  },
+  spl4: {
+    month: `${months[11]}`,
+    494032: spl4dec[0],
+    422386: spl4dec[1],
+    853578: spl4dec[2],
+    709791: spl4dec[3],
+    500995: spl4dec[4],
+    326976: spl4dec[5],
+    767152: spl4dec[6],
+    804806: spl4dec[7],
+    768174: spl4dec[8],
+  },
+  spl5: {
+    month: `${months[11]}`,
+    494032: spl5dec[0],
+    422386: spl5dec[1],
+    853578: spl5dec[2],
+    709791: spl5dec[3],
+    500995: spl5dec[4],
+    326976: spl5dec[5],
+    767152: spl5dec[6],
+    804806: spl5dec[7],
+    768174: spl5dec[8],
+  },
+  spl6: {
+    month: `${months[11]}`,
+    494032: spl6dec[0],
+    422386: spl6dec[1],
+    853578: spl6dec[2],
+    709791: spl6dec[3],
+    500995: spl6dec[4],
+    326976: spl6dec[5],
+    767152: spl6dec[6],
+    804806: spl6dec[7],
+    768174: spl6dec[8],
+  },
+};
 
-// console.log(decSpl);
+allSpl.November = novSpl;
+allSpl.December = decSpl;
+console.log(allSpl);
